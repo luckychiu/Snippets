1f763769a17cb72e3c2929681d8d71ea2a4331d3
Tiger Chiu <luckychiu@163.com>
2016-11-28 17:27:09 +0800

    SnippetData added

---
+++ Snippets/Snippets/SnippetData.swift
@@ -0,0 +1,15 @@
+//
+//  SnippetData.swift
+//  Snippets
+//
+//  Created by Lucky Chiu on 28/11/2016.
+//  Copyright © 2016 ASOKA. All rights reserved.
+//
+
+import Foundation
+
+struct SnippetData {
+    init() {
+        print("new snippet created")
+    }
+}
--- Snippets/Snippets/ViewController.swift
+++ Snippets/Snippets/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var data: [SnippetData] = [SnippetData]()
 
+    @IBAction func createNewSnippet(_ sender: Any) {
+        let newSnippet = SnippetData()
+        data.append(newSnippet)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
