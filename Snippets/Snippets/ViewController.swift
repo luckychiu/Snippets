e50c71af601cb9762b6dffab4c31d331ca0bc8f1
Tiger Chiu <luckychiu@163.com>
2016-11-28 16:08:59 +0800

    Initial Version

---
+++ Snippets/Snippets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Snippets
+//
+//  Created by Lucky Chiu on 28/11/2016.
+//  Copyright © 2016 ASOKA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
